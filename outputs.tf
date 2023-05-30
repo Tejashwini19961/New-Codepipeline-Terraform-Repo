b6ee0f1813734a4d775d4da48ce319759f0e3b99
Tejashwini R <89374814+Tejashwini19961@users.noreply.github.com>
2023-05-30 14:36:08 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "aws_codebuild_project_arn" {
   value       = module.codebuild.aws_codebuild_project_arn
   description = "aws codebuild project arn"
 }
+
+  
+  output "aws_codepipeline_arn" {
+  value       = aws_codepipeline.this.arn
+  description = "aws codepipeline project arn"
+}
